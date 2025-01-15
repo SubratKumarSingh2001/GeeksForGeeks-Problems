842307f5749eb5d2359b852edf79a86b876d15f3
Subrat Kumar Singh <92250219+SubratKumarSingh2001@users.noreply.github.com>
2025-01-15 21:18:14 +0530

    Create MissingInArray.cpp

---
+++ MissingInArray.cpp
@@ -0,0 +1,9 @@
+int missingNumber(vector<int>& arr) {
+    sort(arr.begin(), arr.end());
+    for(int i = 0; i < arr.size(); i++) {
+        if(arr[i] != i + 1) {
+            return i + 1;
+        }
+    }
+    return arr.size() + 1;  
+}
