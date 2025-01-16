af7ec3fc07760cc761fac4f26093a5e3612c664a
Subrat Kumar Singh <92250219+SubratKumarSingh2001@users.noreply.github.com>
2025-01-16 19:24:30 +0530

    Update TransposeOfMatrix.cpp

--- TransposeOfMatrix.cpp
+++ TransposeOfMatrix.cpp
@@ -1,14 +1,10 @@
-class Solution {
-  public:
-    bool matSearch(vector<vector<int>> &mat, int x) {
-        // your code here
-        for(int i=0; i<mat.size(); i++) {
-            for(int j=0; j<mat[0].size(); j++) {
-                if(mat[i][j] == x) {
-                    return true;
-                }
+void transpose(vector<vector<int>>& mat, int n) {
+        int temp;
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j++) {
+                temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
             }
         }
-        return false;
-    }
-};
+}
