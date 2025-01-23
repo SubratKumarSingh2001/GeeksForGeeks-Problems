21e8105b7e0c90a2b4448adcc947ed4348bb8010
Subrat Kumar Singh <92250219+SubratKumarSingh2001@users.noreply.github.com>
2025-01-23 19:24:24 +0530

    Update MajorityElement-1.cpp

--- MajorityElement-1.cpp
+++ MajorityElement-1.cpp
@@ -1,6 +1,7 @@
 class Solution {
   public:
     int majorityElement(vector<int>& arr) {
+        //Method:1 Brute Force Approach TC:O(n^2) and SC:O(1);
         for(int i=0; i<arr.size(); i++) {
             int count=1;
             for(int j=i+1; j<arr.size(); j++) {
@@ -13,5 +14,36 @@ class Solution {
             }
         }
         return -1;
+
+        //Method:2 Optimal Approach (Boyer-More's Voting Algorithm) TC:O(n) and SC:O(1);
+        int ele=0, count=0;
+        for(int i=0; i<arr.size(); i++) {
+            if(count == 0) {
+                ele = arr[i];
+                count++;
+            }
+            else if(arr[i] == ele) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        /*
+        till of now we have got our potential majority element to check whether it is majority elememt we will find the no.of occurence 
+        of ele in array and if it is >size/2 then that potential element is our resultant majority ele
+        */
+        int cnt=0;
+        for(int i=0; i<arr.size(); i++) {
+            if(arr[i] == ele) {
+                cnt++;
+            }
+        }
+        if(cnt > arr.size()/2) {
+            return ele;
+        }
+        else {
+            return -1;
+        }
     }
 };
