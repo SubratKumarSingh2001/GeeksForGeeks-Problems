bb2caffdc028c56dcbd92961e38ac33654543617
Subrat Kumar Singh <92250219+SubratKumarSingh2001@users.noreply.github.com>
2025-01-26 20:56:48 +0530

    Update sort0s1sand2s.cpp

--- sort0s1sand2s.cpp
+++ sort0s1sand2s.cpp
@@ -1,6 +1,12 @@
-void sort012(vector<int>& arr) {
+  public:
+    void sort012(vector<int>& arr) {
         // code here
+        // MethOd:1 
         // sort(arr.begin(), arr.end()); can be sort using inbuilt sort function
+        // TC: O(nlogn) SC: O(n)
+        
+        //Method:2
+        //TC: O(n) SC: O(n)
         int zeros=0, ones=0, twos=0;
         for(int i=0; i<arr.size(); i++) {
             if(arr[i] == 0) zeros++;
@@ -24,5 +30,23 @@ void sort012(vector<int>& arr) {
             i++;
             twos--;
         }
+         
+        //Method:3 optimal solution 
+        //TC: O(n) SC: O(1)
+        int start=0, mid=0, end = arr.size()-1;
+        while(mid <= end) {
+            if(arr[mid] == 0) {
+                swap(arr[start], arr[mid]);
+                start++, mid++;
+            }
+            else if(arr[mid] == 1) {
+                mid++;
+            }
+            else {
+                swap(arr[mid], arr[end]);
+                end--;
+            }
+        }
        
-  }
+    }
+};
