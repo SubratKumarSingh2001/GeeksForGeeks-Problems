c0ffae5963bd490f9ed6d58cb836ea8175e8d08c
Subrat Kumar Singh <92250219+SubratKumarSingh2001@users.noreply.github.com>
2025-02-12 22:02:39 +0530

    Update ReverseDigits.cpp

--- ReverseDigits.cpp
+++ ReverseDigits.cpp
@@ -1,11 +1,10 @@
 int reverseDigits(int n) {
         // Code here
+        // TC:O(n^2) SC:O(n)
         int num=0; 
         while(n) {
             int rem = n%10;
-            if(rem != 0) {
-                num = num*10 + rem;
-            }
+            num = num*10 + rem;
             n = n/10;
         }
         return num;
