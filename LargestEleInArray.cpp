67eb2cf6141d64707e2c4fd16e5013b74ae66e2d
Subrat Kumar Singh <92250219+SubratKumarSingh2001@users.noreply.github.com>
2025-06-21 13:51:12 +0530

    Update LargestEleInArray.cpp

--- LargestEleInArray.cpp
+++ LargestEleInArray.cpp
@@ -2,6 +2,7 @@ class Solution {
   public:
     int largest(vector<int> &arr) {
         // code here
+        // TC: O(n), SC: O(1)
         int largest = INT_MIN;
         for(int i=0; i<arr.size(); i++) {
             if(arr[i] > largest) {
